677d0d8b0cd4759ae9e7b08f63683883825c0db2
mydongistiny <jaysonedson@gmail.com>
2015-10-24 02:19:13 -0700

    Import Google Messenger 1.5.037(2294727-30)

---
+++ Android.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (c) 2015 Benzo Rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := messaging
+LOCAL_SRC_FILES := messaging.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+$(shell mkdir -p $(TARGET_OUT_APPS)/messaging/lib/arm)
+$(shell cp $(LOCAL_PATH)/lib/armeabi-v7a/libframesequence.so $(TARGET_OUT_APPS)/messaging/lib/arm/libframesequence.so)
+$(shell cp $(LOCAL_PATH)/lib/armeabi-v7a/libgiftranscode.so $(TARGET_OUT_APPS)/messaging/lib/arm/libgiftranscode.so)
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(TARGET_OUT_APPS)/messaging/lib/arm/libframesequence.so \
+	$(TARGET_OUT_APPS)/messaging/lib/arm/libgiftranscode.so
+include $(BUILD_PREBUILT)
